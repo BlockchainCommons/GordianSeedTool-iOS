73a9ca8b885937d887195b0125298027af9256e2
Wolf McNally <wolf@wolfmcnally.com>
2020-12-11 03:54:52 -0800

    Refactor.

---
+++ Fehu/Tokens/TokenStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TokenStyle.swift
+//  Fehu
+//
+//  Created by Wolf McNally on 12/11/20.
+//
+
+import SwiftUI
+
+struct TokenStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(5)
+            .background(Color.gray.opacity(0.7))
+            .cornerRadius(5)
+    }
+}
+
+extension View {
+    func tokenStyle() -> some View {
+        modifier(TokenStyle())
+    }
+}
--- Fehu/ValueViewable.swift
+++ Fehu/ValueViewable.swift
@@ -11,23 +11,3 @@ protocol ValueViewable {
     static var minimumWidth: CGFloat { get }
     var view: AnyView { get }
 }
-
-protocol StringTransformable {
-    static func values(from string: String) -> [Self]?
-    static func string(from values: [Self]) -> String
-}
-
-struct TokenStyle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding(5)
-            .background(Color.gray.opacity(0.7))
-            .cornerRadius(5)
-    }
-}
-
-extension View {
-    func tokenStyle() -> some View {
-        modifier(TokenStyle())
-    }
-}
